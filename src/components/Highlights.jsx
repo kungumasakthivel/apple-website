d97a7fb81ce91a68399abec04978784de799514d
kungumasakthivel <kungumasakthivel.k@gmail.com>
2025-01-16 23:27:05 +0530

    added video carousel

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,43 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { rightImg, watchImg } from "../utils"
+import VideoCarousel from "./VideoCarousel"
+
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to('#title', {
+      opacity: 1,
+      y:0
+    })
+    gsap.to('.link', {
+      opacity: 1,
+      y: 0,
+      stagger: 0.25
+    })
+  })
   return (
-    <div>
+    <section id="highlights" className="w-screen overflow-hidden h-full
+    common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div className="mb-12 w-full items-end md:flex justify-between">
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+          <div className="flex flex-wrap items-end gap-5 mt-2">
+            <p className="link">
+              Watch the film
+              <img src={watchImg} alt="watch" className="ml-2" />  
+            </p>
+            <p className="link">
+              Watch the film
+              <img src={rightImg} alt="right" className="ml-2" />  
+            </p>
+          </div>
+        </div>
+        <VideoCarousel />
+      </div>
       
-    </div>
+    </section>
   )
 }
 
